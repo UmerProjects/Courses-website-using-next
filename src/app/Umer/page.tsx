1e2f61c1b30f62fd49f2a7ea285220e93f5bcea3
Umer qazi <uqazi844@gmail.com>
2024-08-30 23:15:27 +0500

    first commit

---
+++ src/app/Umer/page.tsx
@@ -0,0 +1,5 @@
+export default function Umer () {
+    return (
+        <h1 className="text-3xl text-center mt-8">Hello World</h1>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingSeminars from "@/components/UpcomingSeminars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.9] antialiased bg-grid-white[0.02]">
+    <HeroSection />
+    <FeaturedCourses />
+    <WhyChooseUs/>
+    <TestimonialCard/>
+    <UpcomingSeminars/>
+    <Instructors/>
+    <Footer/>
+    </main>
+  );
+}
